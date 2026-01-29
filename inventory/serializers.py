c193757094ff2f61fe4e45363bb375422eaeccbb
Fernando Castro <fcastropro@gmail.com>
2026-01-28 23:41:26 -0500

    realizamos cambios en las relaciones

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
+from catalog.models import Product
 from .models import Inventory
 
+
+class ProductNestedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('id', 'name')
+
 class InventorySerializer(serializers.ModelSerializer):
+    product = ProductNestedSerializer(read_only=True)
+    product_id = serializers.PrimaryKeyRelatedField(
+        source='product',
+        queryset=Product.objects.all(),
+        write_only=True
+    )
+
     class Meta:
         model = Inventory
         fields = '__all__'
